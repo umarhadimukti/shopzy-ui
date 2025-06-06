9936516affdbdbf3939d32f448fe15612e02a0bd
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-06 22:20:50 +0700

    added function error handler

---
+++ app/utils/error.ts
@@ -0,0 +1,13 @@
+export const getErrorMessage = (response: any): string => {
+    if (response.message) {
+        if (Array.isArray(response.message)) {
+            return formatErrorMessage(response.message[0]);
+        }
+    }
+
+    return 'unknown error occured';
+}
+
+const formatErrorMessage = (message: string): string => {
+    return `${message.charAt(0).toUpperCase()} ${message.slice(1)}`;
+}
