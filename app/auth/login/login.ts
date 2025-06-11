32e63806951d9e8c95d6238599cbb7ed94be9639
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-11 21:45:17 +0700

    store auth token in cookies

--- app/auth/login/login.ts
+++ app/auth/login/login.ts
@@ -3,11 +3,13 @@
 import { API_URL } from "@/app/constants/api";
 import { getErrorMessage } from "@/app/utils/fetch";
 import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
 
 export default async function login(_prevState: FormError, formData: FormData) {
     const res = await fetch(`${API_URL}/api/auth/login`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(Object.fromEntries(formData)),
     });
 
@@ -17,8 +19,22 @@ export default async function login(_prevState: FormError, formData: FormData) {
         return { error: getErrorMessage(parsedRes) };
     }
 
-    const token = res.headers.getSetCookie()[0].split(';')[0].split('=')[1];
-    console.log(token)
+    setAuthCookie(res);
 
     redirect("/");
+}
+
+const setAuthCookie = async (res: Response) => {
+    const setCookieHeader = res.headers.getSetCookie();
+    if (setCookieHeader) {
+        const token = setCookieHeader[0].split(";")[0].split("=")[1];
+        const cookieStore = await cookies();
+        cookieStore.set({
+            name: "Authentication",
+            value: token,
+            secure: true,
+            httpOnly: true,
+            expires: new Date(jwtDecode(token).exp! * 1000),
+        });
+    }
 }
