4c35653ded88a0b20b490c023e71057c5f54a21b
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-14 11:43:01 +0700

    refactor authenticated in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { NextRequest } from "next/server";
+import authenticated from "./app/auth/authenticated";
 
 const unauthorizedRoutes: string[] = ["/auth/login", "/auth/signup"];
 
 export function middleware(request: NextRequest) {
-    const auth = request.cookies.get("Authentication")?.value;
-
-    if (!auth && !unauthorizedRoutes.some(route => request.nextUrl.pathname.startsWith(route))) {
+    if (!authenticated() && !unauthorizedRoutes.some(route => request.nextUrl.pathname.startsWith(route))) {
         return Response.redirect(new URL("/auth/login", request.url));
     }
 }
