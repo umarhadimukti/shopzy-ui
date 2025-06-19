b8db0bcc9729ebb5a6d8df4f942526f9d873334f
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-19 19:40:22 +0700

    added style in layout when authenticated

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
                 <Providers authenticated={isAuthenticated}>
                     <CssBaseline/>
                         <Header logout={logout}/>
-                    <Container className="mt-8">{children}</Container>
+                    <Container className={isAuthenticated ? "mt-10" : ""}>{children}</Container>
                 </Providers>
             </body>
         </html>
