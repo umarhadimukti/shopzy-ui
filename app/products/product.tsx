a35bd82f4d5e7555df5681d0763430c1b05e5ec5
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-23 13:57:41 +0700

    show product image

--- app/products/product.tsx
+++ app/products/product.tsx
@@ -1,5 +1,7 @@
-import { Card, Typography } from "@mui/material";
+import { Card, Typography, Stack } from "@mui/material";
 import { Product as IProduct } from "./interfaces/product.interface";
+import Image from "next/image";
+import { API_URL } from "../common/constants/api";
 
 interface ProductProps {
     product: IProduct;
@@ -8,9 +10,19 @@ interface ProductProps {
 export default function Product({ product }: ProductProps) {
     return (
         <Card className="p-4">
-            <Typography variant="h6">{product.name}</Typography>
-            <Typography variant="body2">{product.description}</Typography>
-            <Typography variant="body2">{product.price}</Typography>
+            <Stack gap={3}>
+                <Image
+                    src={`${API_URL}/products/`}
+                    width={0}
+                    height={0}
+                    className="w-full h-auto"
+                    sizes="100vw"
+                    alt={`img-${product.id}`}
+                />
+                <Typography variant="h6">{product.name}</Typography>
+                <Typography variant="body2">{product.price}</Typography>
+                <Typography variant="body2">{product.description}</Typography>
+            </Stack>
         </Card>
     )
 }
