81f11826c238be5d7a7ce11c82bb90807baf0a0e
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-25 14:55:34 +0700

    styling width of layout in product

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import { getProduct } from "./get-product";
 import Image from "next/image";
 import { API_URL } from "@/app/common/constants/api";
@@ -13,24 +13,28 @@ export default async function SingleProduct({ params }: SingleProductProps) {
     const product = await getProduct(+params.productId);
 
     return (
-        <Stack gap={2} marginBottom="3rem">
-            <Typography variant="h5" sx={{fontWeight: "bold"}}>{product.data?.name}</Typography>
-            {
-                product.data?.imageExists && (
-                    <Image
-                        src={`${API_URL}/images/products/${product.data?.id}.jpg`}
-                        width={0}
-                        height={0}
-                        className="w-auto md:w-1/2 h-auto"
-                        sizes="100vw"
-                        alt={`img-${product.data?.id}-product`}
-                    />
-                )
-            }
-            <Stack gap={1}>
-                <Typography variant="h5" sx={{fontWeight: 300}}>{product.data?.price}</Typography>
-                <Typography variant="body1" sx={{fontWeight: 300, color: "text.secondary"}} className="truncate">{product.data?.description}</Typography>
-            </Stack>
-        </Stack>
+        <Grid container spacing={1}>
+            <Grid size={{xs: 12, md: 6}}>
+                {
+                    product.data?.imageExists && (
+                        <Image
+                            src={`${API_URL}/images/products/${product.data?.id}.jpg`}
+                            width={0}
+                            height={0}
+                            className="w-auto md:w-11/12 h-auto"
+                            sizes="100vw"
+                            alt={`img-${product.data?.id}-product`}
+                        />
+                    )
+                }
+            </Grid>
+            <Grid size={{xs: 12, md: 6}}>
+                <Stack gap={1}>
+                    <Typography variant="h5" sx={{fontWeight: "bold"}}>{product.data?.name}</Typography>
+                    <Typography variant="h5" sx={{fontWeight: 300}}>{product.data?.price}</Typography>
+                    <Typography variant="body1" sx={{fontWeight: 300, color: "text.secondary"}} className="truncate">{product.data?.description}</Typography>
+                </Stack>
+            </Grid>
+        </Grid>
     )
 }
