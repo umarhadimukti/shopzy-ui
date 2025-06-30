2a15b65a491399c05ef19ff14eabc95e8dc23d93
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-30 19:41:52 +0700

    added handler in middleware when user is authenticated

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -155,7 +155,7 @@ const Settings = ({ logout }: HeaderProps) => {
         <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
+                <Avatar alt="Remy Sharp" src="/person.png" sx={{ padding: "5px" }} />
                 </IconButton>
             </Tooltip>
             <Menu
--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,16 @@ import authenticated from "./app/auth/authenticated";
 export async function middleware(request: NextRequest) {
     const auth = await authenticated();
 
+    // handle when user is not authenticated
     if (!auth && !unauthenticatedRoutes.some(route => request.nextUrl.pathname.startsWith(route.path))) {
         return Response.redirect(new URL("/auth/login", request.url));
+    }
+
+    // handle when user is authenticated
+    const pathname = request.nextUrl.pathname;
+    const isAuthRoute = pathname.startsWith("/auth/login") || pathname.startsWith("/auth/register");
+    if (auth && isAuthRoute) {
+        return Response.redirect(new URL("/", request.url));
     }
 }
 
