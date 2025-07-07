21bcb96fba186544e719f88c10c9900bebb3571d
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-07-07 07:22:25 +0700

    remove unused import

--- app/products/products.tsx
+++ app/products/products.tsx
@@ -1,7 +1,4 @@
 import getProducts from "./actions/get-products";
-import { Product as IProduct } from "./interfaces/product.interface";
-import Product from "./product";
-import { Grid } from "@mui/material";
 import ProductsGrid from "./products-grid";
 
 export default async function Products() {
