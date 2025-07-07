03fa5cc898040a58381030f4b939de59a8a7a8b8
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-07-07 15:53:22 +0700

    added socket and revalidate products websocket

--- app/products/products-grid.tsx
+++ app/products/products-grid.tsx
@@ -3,12 +3,28 @@
 import { Grid } from "@mui/material";
 import { Product as IProduct } from "./interfaces/product.interface";
 import Product from "./product";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import { API_URL } from "../common/constants/api";
+import revalidateProducts from "./actions/revalidate-products";
 
 interface ProductsGridProps {
     products?: IProduct[],
 }
 
 export default function ProductsGrid({ products }: ProductsGridProps) {
+    useEffect(() => {
+        const socket = io(API_URL!);
+
+        socket.on("productUpdated", () => {
+            revalidateProducts();
+        });
+
+        return () => {
+            socket?.disconnect();
+        }
+    }, []);
+    
     return (
         <Grid
             container
