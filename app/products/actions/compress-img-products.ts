1b177f36ec40c19b6dfe9a8fa780c86c511e600e
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-07-14 23:51:23 +0700

    fix compress file

--- app/products/actions/compress-img-products.ts
+++ app/products/actions/compress-img-products.ts
@@ -3,9 +3,9 @@ import { gzip } from "pako";
 export default async function compressImageProducts(file: File): Promise<Blob | { error: string }> {
     try {
         const arrayBuffer = await file.arrayBuffer();
-        const compressedData = gzip(new Uint8Array(arrayBuffer), { level: 9 });
+        const compressedData = gzip(new Uint8Array(arrayBuffer));
         
-        return new Blob([compressedData], { type: "application/gzip" });
+        return new Blob([compressedData], { type: "application/octet-stream" });
     } catch (error) {
         console.error(`Error compressing image products: ${error}`);
         return { error: "Failed to compress image" };
--- app/products/actions/create-product.ts
+++ app/products/actions/create-product.ts
@@ -4,7 +4,6 @@ import { revalidateTag } from "next/cache";
 import { post } from "../../common/utils/fetch";
 import { API_URL } from "@/app/common/constants/api";
 import { cookies } from "next/headers";
-import compressImageProducts from "./compress-img-products";
 
 export default async function createProduct(formData: FormData) {
     const response = await post("api/products/new", formData);
@@ -21,13 +20,8 @@ export default async function createProduct(formData: FormData) {
 }
 
 async function uploadProductImage(productId: number, file: File) {
-    const compressedProductImage = await compressImageProducts(file);
     const formData = new FormData();
-    formData.append(
-        "image",
-        compressedProductImage instanceof Blob ? compressedProductImage : file,
-        `${file.name}.gz`,
-    );
+    formData.append("image", file);
 
     const cookieStore = await cookies();
     const cookieString = cookieStore.toString();
